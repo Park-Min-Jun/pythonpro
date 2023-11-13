afc1ff60ff1b1fd22fd9da2358d5487d17696ad6
minjun <gomdole3963@gmail.com>
2023-11-13 12:22:45 +0000

    ,

---
+++ chap8/prob5.py
@@ -0,0 +1,56 @@
+try:
+
+    num = float (input ("Enter a number: "))
+except ValueError:
+
+    print("Something went wrong!")
+
+try:
+
+    num = float(input("\nEnter a number: "))
+except ValueError:
+
+    print("That was not a number!\n")
+
+for value in (None, "Hi!"):
+
+    try:
+        print("Attempting to convert", value, "->", end=" ")
+        print(float(value))
+
+    except (TypeError, ValueError):
+ 
+        print("Something went wrong!")
+
+print()
+
+for value in (None, "Hi!"):
+
+    try:
+        print("Attempting to convert", value, "->", end=" ")
+        print(float(value))
+    
+    except TypeError:
+        print("Can only convert string or number!")
+    
+    except ValueError:
+        print("Can only convert a string of digits!")
+
+try:
+    
+    num = float(input("\nEnter a number: "))
+
+except ValueError as e:
+    
+    print("Not a number! or as Python would say\n", e)
+
+
+try:
+
+    num = float(input("\nEnter a number: "))
+
+except ValueError:
+    print("That was not a number!")
+
+else:
+    print("You entered the number", num)
